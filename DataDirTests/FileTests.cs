ba6d5892f20f2c01d69a39a95ee673cd285bb923
Miff Falden <miff@miffthefox.info>
2018-09-02 17:39:09 -0400

    Add file access

---
+++ DataDirTests/FileTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MiffTheFox;
+using MiffTheFox.DataDirs;
+
+namespace DataDirTests
+{
+    [TestClass]
+    public class FileTests
+    {
+        [TestMethod]
+        public void TestOpenFile()
+        {
+            var contents = BinString.FromTextString("Hello, world!\n", System.Text.Encoding.ASCII);
+            var dd = DataDir.Create(DataDirType.Temporary);
+
+            string filename = DateTime.Now.Ticks.ToString("x") + ".tmp";
+            string fullFilename = Path.Combine(dd.Path, filename);
+
+            Assert.IsFalse(File.Exists(fullFilename));
+            Assert.ThrowsException<FileNotFoundException>(() => dd.OpenFile(filename, FileMode.Open, FileAccess.Read));
+
+            using (var stream = dd.OpenFile(filename, FileMode.Create, FileAccess.ReadWrite))
+            {
+                stream.Write(contents);
+                stream.Close();
+            }
+
+            Assert.IsTrue(File.Exists(fullFilename));
+            Assert.AreEqual(contents, (BinString)File.ReadAllBytes(fullFilename));
+
+            using (var stream = dd.OpenFile(filename, FileMode.Open, FileAccess.Read))
+            {
+                var read = stream.ReadBinString(contents.Length);
+                Assert.AreEqual(contents, read);
+            }
+
+            File.Delete(fullFilename);
+
+            Assert.ThrowsException<ArgumentException>(() => dd.OpenFile(null, FileMode.Create, FileAccess.ReadWrite));
+            Assert.ThrowsException<ArgumentException>(() => dd.OpenFile(string.Empty, FileMode.Create, FileAccess.ReadWrite));
+        }
+
+        [TestMethod]
+        public void TestSimpleReadWrite()
+        {
+            string stringData = "Hello, world! This is a string.\n";
+            var binData = BinString.FromBytes("09F911029D74E35BD84156C5635688C0");
+
+            var dd = DataDir.Create(DataDirType.Temporary);
+
+            dd.WriteAllText("test.txt", stringData, System.Text.Encoding.ASCII);
+            Assert.AreEqual(stringData, dd.ReadAllText("test.txt", Encoding.ASCII));
+
+            dd.WriteAllBytes("test.bin", binData);
+            Assert.AreEqual(binData, dd.ReadAllBytes("test.bin"));
+        }
+    }
+}
---
+++ MiffTheFox.DataDirs/DataDirFileAccess.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using MiffTheFox;
+using PathType = System.IO.Path;
+
+namespace MiffTheFox.DataDirs
+{
+    partial class DataDir
+    {
+        public Stream OpenFile(string filePath, FileMode mode, FileAccess access)
+        {
+            if (string.IsNullOrEmpty(filePath)) throw new ArgumentException(nameof(filePath), "File path cannot be null or empty.");
+
+            string fullPath = PathType.Combine(Path, filePath);
+            string dir = PathType.GetDirectoryName(fullPath);
+            bool createIfNotExists = false;
+
+            if (access != FileAccess.Read)
+            {
+                switch (mode)
+                {
+                    case FileMode.Append:
+                    case FileMode.Create:
+                    case FileMode.CreateNew:
+                    case FileMode.OpenOrCreate:
+                        createIfNotExists = true;
+                        break;
+                }
+            }
+
+            if (!Directory.Exists(dir))
+            {
+                if (createIfNotExists)
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                else
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+
+            return File.Open(fullPath, mode, access);
+        }
+
+        public BinString ReadAllBytes(string filePath)
+        {
+            using (var stream = OpenFile(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var buffer = new MemoryStream())
+                {
+                    stream.CopyTo(buffer);
+                    return buffer.ToBinString();
+                }
+            }
+        }
+
+        public void WriteAllBytes(string filePath, BinString data)
+        {
+            using (var stream = OpenFile(filePath, FileMode.Create, FileAccess.ReadWrite))
+            {
+                stream.Write(data);
+            }
+        }
+
+        public string ReadAllText(string filePath, Encoding encoding) => ReadAllBytes(filePath).ToString(encoding);
+        public void WriteAllText(string filePath, string data, Encoding encoding) => WriteAllBytes(filePath, BinString.FromTextString(data, encoding));
+    }
+}
